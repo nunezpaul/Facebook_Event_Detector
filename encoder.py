ca878762ee3a839913e9d609889678592cf26e55
Paul Nunez <p@Pauls-MacBook-Pro.local>
2018-01-09 19:24:24 -0800

    encoder update

--- encoder.py
+++ encoder.py
@@ -1,5 +1,15 @@
+import pickle
 if __name__ == '__main__':
     fileName = 'labeledData/cleanedEvents.p'
     events = pickle.load(open(fileName, 'rb'))
     encodedEvents = [map(ord, event) for event in events]
-    pickle.dump( events, open('labeledData/encodedEvents.p', "wb" ) )
+    for i in range(len(encodedEvents)):
+        if len(encodedEvents[i]) > 64:
+            encodedEvents[i] = encodedEvents[i][:64]
+        else:
+            hold = [0]*64
+            hold[:len(encodedEvents[i])] = encodedEvents[i]
+            encodedEvents[i] = hold
+
+    pickle.dump( encodedEvents, open('labeledData/encodedEvents.p', "wb" ) )
+
